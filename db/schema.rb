b0a52b012dc1a51369bf6b54f683ddc83a3eaff7
joelle5 <ng_joelle@yahoo.com>
2017-11-08 22:24:00 +0800

    drop table :items

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108060253) do
+ActiveRecord::Schema.define(version: 20171108141844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "items", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "firstname"
