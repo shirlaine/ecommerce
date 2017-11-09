5549ca057137130344d4f577b1e6f691a02661c0
shirlaine <shirlaine.phang@gmail.com>
2017-11-09 21:56:08 +0800

    Add user and admin factories and model specs

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,4 +5,8 @@ class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :username, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :username, presence: true
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
