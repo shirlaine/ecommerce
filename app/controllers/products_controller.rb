16e12ff720c7dd44773f3e270bdd62676461e618
shirlaine <shirlaine.phang@gmail.com>
2017-11-09 21:31:36 +0800

    Generate views and routes for devise admin, add custom field to views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,8 +9,12 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.create(product_params)
-    redirect_to root_url
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,19 +24,17 @@ class ProductsController < ApplicationController
   def update
     @products = Product.all
     @product = Product.find(params[:id])
-    if
-      @product.update_attributes(post_params)
-      redirect_to root_url
+    if @product.update(product_params)
+      redirect_to products_path
     else
       render :edit
     end
-
   end
 
+  private
 
-
-private
   def product_params
     params.require(:product).permit(:name, :description, :image)
   end
+
 end
