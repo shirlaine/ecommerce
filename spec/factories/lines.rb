6d04ff4dc31bfed603d1490a7a0a42d30f6a5dbf
shirlaine <shirlaine.phang@gmail.com>
2017-11-15 23:56:46 +0800

    Remove column :quantity in Line model

--- spec/factories/lines.rb
+++ spec/factories/lines.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :line do
     product
     price 5.50
-    quantity 5
     size 0
   end
 end
--- spec/models/line_spec.rb
+++ spec/models/line_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe Line, type: :model do
   let (:subject) { create(:line) }
   it { expect(subject).to validate_presence_of(:price) }
-  it { expect(subject).to validate_presence_of(:quantity) }
   it { expect(subject).to belong_to(:product) }
 end
