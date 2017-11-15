1deb3e3f51fbf66e40db2b7591cfcb8e989fc32c
noena <noena_deleon@yahoo.com>
2017-11-15 16:21:29 +0800

    rectified testing error

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 20171114160808) do
     t.bigint "line_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quantity"
     t.index ["line_id"], name: "index_cart_items_on_line_id"
     t.index ["user_id"], name: "index_cart_items_on_user_id"
   end
@@ -54,6 +53,7 @@ ActiveRecord::Schema.define(version: 20171114160808) do
 
   create_table "lines", force: :cascade do |t|
     t.decimal "price", null: false
+    t.integer "quantity", null: false
     t.integer "size", default: 0
     t.bigint "product_id"
     t.datetime "created_at", null: false
