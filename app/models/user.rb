408c3409dbe0551cfea8c78f1f56e8f23d18699f
shirlaine <shirlaine.phang@gmail.com>
2017-11-16 01:56:45 +0800

    Add methods for adding products into cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,30 @@ class User < ApplicationRecord
     end
   end
 
+  def cart_total_price
+    total_price = 0
+    get_cart_products.each { |product| total_price += product.price }
+    total_price
+  end
+
+  def get_cart_products
+    current_user.lines
+  end
+
+  def purchase_cart_products!
+    get_cart_products.each { |product| purchase(product)}
+    Cart.destroy(current_user.cart.id)
+  end
+
+  def purchase(product)
+    lines << product unless purchase?(product)
+  end
+
+  def purchase?(product)
+    lines.include?(product)
+  end
+
 end
+
+
+
