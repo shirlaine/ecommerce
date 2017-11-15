3f125b7881d755847bd2cc4027347bd739969dbe
shirlaine <shirlaine.phang@gmail.com>
2017-11-15 23:55:24 +0800

    Rubocop fixes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,17 +8,15 @@ class CartsController < ApplicationController
   end
 
   def add
-    @cart_item = CartItem.create(user_id:current_user.id, line_id: params[:line_id])
-    flash[:notice]='Item has been added to cart'
+    @cart_item = CartItem.create(user_id: current_user.id, line_id: params[:line_id])
+    flash[:notice] = 'Item has been added to cart'
     redirect_to root_path
   end
 
   def remove
-    @cart_item= CartItem.find_by(line_id: params[:line_id]).destroy
-    flash[:alert]='Item has been deleted from cart'
+    @cart_item = CartItem.find_by(line_id: params[:line_id]).destroy
+    flash[:alert] = 'Item has been deleted from cart'
     redirect_to cart_path(current_user.cart)
   end
 
 end
-
-
