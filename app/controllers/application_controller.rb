2c2e5c2561e849f20b743ef748be5699700d5417
shirlaine <shirlaine.phang@gmail.com>
2017-11-16 22:49:08 +0800

    Add sessions cart to application and carts controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   before_action :set_current_user
-  before_action :set_cart
+  before_action :set_cart_user
 
   protected
 
@@ -21,11 +21,20 @@ class ApplicationController < ActionController::Base
     User.current_user = current_user
   end
 
-  def set_cart
+  def set_cart_user
     if user_signed_in?
       Cart.create!(user_id: current_user.id) if current_user.cart.blank?
     end
     @cart = current_user.cart if user_signed_in?
+  end
+
+  def session_cart
+    if session['cart'].nil?
+      session['cart'] = []
+    else
+      session['cart']
+    end
+    # session['cart'] ||session_cart= []
   end
 
 end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,22 +1,35 @@
 class CartsController < ApplicationController
 
-  before_action :authenticate_user!
-  before_action :set_cart, only: [:add, :remove, :show]
+  before_action :set_cart_user, only: [:add, :remove, :show]
 
   def show
-    @products = current_user.lines
+    if current_user
+      @products = current_user.lines
+    else
+      # setting a session cart to session_cart_id
+      session_cart_id = session_cart
+      @products = Line.find(session_cart_id)
+    end
   end
 
   def add
-    @cart_item = CartItem.create(user_id: current_user.id, line_id: params[:line_id])
+    if current_user
+      @cart_item = CartItem.create(user_id: current_user.id, line_id: params[:line_id])
+    else
+      session_cart << params[:line_id].to_i
+    end
     flash[:notice] = 'Item has been added to cart'
-    redirect_to root_path
+    redirect_to cart_path
   end
 
   def remove
-    @cart_item = CartItem.find_by(line_id: params[:line_id]).destroy
+    if current_user
+      @cart_item = CartItem.find_by(line_id: params[:line_id]).destroy
+    else
+      session_cart.delete_at(session_cart.find_index(params[:line_id].to_i))
+    end
     flash[:alert] = 'Item has been deleted from cart'
-    redirect_to cart_path(current_user.cart)
+    redirect_to cart_path
   end
 
 end
